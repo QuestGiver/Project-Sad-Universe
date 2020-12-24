08d9cc3a4c97d9850298235fca7ad55b0e6a6dce
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2020-12-23 16:25:50 -0800

    Initial commit

---
+++ Assets/Scenes/Scripts/CombatManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    private int Round;//Current Round
+    private int Turn;//Current turn
+    private int Reactions;//number of reactions left to cycle through
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int[] InitializeTurnOrder(int[] _speedStatArray)
+    {
+        for (int i = 0; i < _speedStatArray.Length; i++)
+        {
+            if (i + 1 < _speedStatArray.Length)
+            {
+                if (_speedStatArray[i] > _speedStatArray[i + 1])
+                {
+                    _speedStatArray[i] += _speedStatArray[i + 1];
+                    _speedStatArray[i + 1] -= _speedStatArray[i];
+                    _speedStatArray[i] -= _speedStatArray[i + 1];
+                    i = 0;
+                }
+            }
+        }
+
+        return _speedStatArray;
+    }
+
+    private void TurnManager()//should be a coroutine
+    {
+
+    }
+
+    private void RoundManager()//should be a coroutine
+    {
+
+    }
+
+    private void ReactionCounter()//should be a coroutine
+    {
+        //event logic
+    }
+
+}
---
+++ Assets/Scenes/Scripts/UIScripting/MainCombatDropdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCombatDropdown : MonoBehaviour
+{
+    public Dropdown MainCombatDropMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ParseMenuSelection()
+    {
+        switch(MainCombatDropMenu.value)
+        {
+            case 0:
+                WeaponsMenu();
+                break;
+
+            default:
+
+                break;
+
+        }
+    }
+
+    public void WeaponsMenu()
+    {
+        Debug.Log("I am A fucking goomba");
+    }
+}
