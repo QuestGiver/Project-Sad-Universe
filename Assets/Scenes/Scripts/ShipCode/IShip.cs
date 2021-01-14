e58984b5752b45d364f9bbf9b9e1d0e22ad63006
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2021-01-14 11:41:07 -0800

    add a many many more core ship stats

--- Assets/Scenes/Scripts/ShipCode/IShip.cs
+++ Assets/Scenes/Scripts/ShipCode/IShip.cs
@@ -6,14 +6,30 @@ public interface IShip
 {
     void ShipAttack();
     void UpdatePosition();
+    void CheckPowerDist();
     void ShipDestroy();
 }
 
 public struct ShipStats
 {
-    public int HpMax;//aka max structural integrity
-    public int HP;//aka current structural integrity
-    public int SpeedMax;
-    public int Speed;
-
+    public int HpMax;//max structural integrity
+    public int HP;//current structural integrity
+    public int SpeedMax;//maximum speed value for ship
+    public int Speed;//Total speed after calculations
+    public int ChargeMax;//maximum action points
+    public int Charge;//current action points availible
+    public int Evasion;//how evasive the ship current is
+    public int EvasionMax;//how evasive the ship can be
+    public int Dissipation;//how much heat the ship can currently dissipate per turn
+    public int DissipationMax;//the maximum value the ship can obtain for dissipation
+    public int Luck;//current critial hit chance modifier and accuracy modifier
+    public int LuckMax;//the maximum ammount of luck the ship may have
+    public int Resistance;//the current percentage of damage that the ships hull can resist
+    public int ResistanceMax;//maximum resistance percentage the ship allows
+    public int Heat;//the current heat level of the ship
+    public int HeatMax;//The maximum ammount of heat the ship can gain before taking damage
+    public int Shields;//The current strength of the ships shields
+    public int ShieldMax;//The maximum strength of the ships shield, determined by "((shield generator size/ship size) * shield rating) * (100 * power distributer allocation)" //power distributor allocation should range from 0.25-2
+    public int PwrSupply;//the rate by which Charge regenerates per turns
+    public int PwrSupplyMax;//The maximum, and probably normal, Charge regeneration ammount
 }
--- Assets/Scenes/Scripts/ShipCode/PlayerShip.cs
+++ Assets/Scenes/Scripts/ShipCode/PlayerShip.cs
@@ -6,19 +6,28 @@ public class PlayerShip : MonoBehaviour, IShip
 {
     IShipEquipment ShipAttackBehavior;
 
+    //execute ship destruction behavior
     public void ShipDestroy()
     {
         throw new System.NotImplementedException();
     }
 
+    //update ship position on radar
     public void UpdatePosition()
     {
         throw new System.NotImplementedException();
     }
 
+    //execute attack behavior
     public void ShipAttack()
     {
         ShipAttackBehavior.Activate();
+    }
+
+    //check power distibution and update relevant statistics
+    public void CheckPowerDist()
+    {
+        throw new System.NotImplementedException();
     }
 
     // Start is called before the first frame update
@@ -32,4 +41,6 @@ public class PlayerShip : MonoBehaviour, IShip
     {
         
     }
+
+
 }
