48d8f382eccbb875dac6874abfa76e1c5a29c6b9
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2021-06-08 14:27:34 -0700

    Began work on observer patter classes for UI

---
+++ Assets/Scenes/Scripts/Observable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observable : IObservable
+{
+    public void addObverver()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void notifyObserver()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void removeObserver()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scenes/Scripts/Observer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : IObserver
+{
+    public void oUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
